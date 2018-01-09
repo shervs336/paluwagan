eb86a365209e918a8e6eac21866311da63378437
Sherwin Rhey Condez <shervs336@gmail.com>
2018-01-09 15:08:56 +0800

    Apply bootswatch layout in application

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Sta Maria. Paluwagan 2018</title>
+
+        <link rel="stylesheet" href="https://bootswatch.com/3/flatly/bootstrap.min.css">
+
+    </head>
+    <body>
+        <div class="container">
+            <div class="content">
+                @yield('content')
+            </div>
+        </div>
+
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
+    </body>
+</html>
