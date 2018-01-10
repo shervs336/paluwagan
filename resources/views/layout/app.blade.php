2d04303c0db6040f7841c76790d41e0dc68084c3
Sherwin Rhey Condez <shervs336@gmail.com>
2018-01-10 15:28:24 +0800

    Add jquery reference

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
         <title>Sta Maria. Paluwagan 2018</title>
@@ -62,6 +62,7 @@
             </div>
         </div>
 
+        <script src="https://code.jquery.com/jquery-3.2.1.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" ></script>
     </body>
 </html>
