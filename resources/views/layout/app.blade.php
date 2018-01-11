0c23ee4a871508b2dc3e6c4c4a344ed7d624eba0
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2018-01-12 01:58:58 +0800

    update css reference

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,7 +9,7 @@
 
         <link rel="stylesheet" href="https://bootswatch.com/3/flatly/bootstrap.min.css" />
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}" />
+        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
     </head>
     <body>
 
